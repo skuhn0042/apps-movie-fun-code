da0cb41654b0611ac1d984a9970dd4f1e1905b13
pal_user <saideepthi.guntha@perficient.com>
2018-02-28 15:38:34 -0700

    Sprint Bootification lab

---
+++ src/main/java/org/superbiz/moviefun/HomeController.java
@@ -0,0 +1,39 @@
+package org.superbiz.moviefun;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+
+@Controller
+public class HomeController {
+
+    private MoviesBean moviesBean;
+
+    @Autowired
+    public HomeController(MoviesBean moviesBean){
+        this.moviesBean = moviesBean;
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @RequestMapping("/setup")
+    public String setup(Map<String, Object> model){
+        moviesBean.addMovie(new Movie("Wedding Crashers", "David Dobkin", "Comedy", 7, 2005));
+        moviesBean.addMovie(new Movie("Starsky & Hutch", "Todd Phillips", "Action", 6, 2004));
+        moviesBean.addMovie(new Movie("Shanghai Knights", "David Dobkin", "Action", 6, 2003));
+        moviesBean.addMovie(new Movie("I-Spy", "Betty Thomas", "Adventure", 5, 2002));
+        moviesBean.addMovie(new Movie("The Royal Tenenbaums", "Wes Anderson", "Comedy", 8, 2001));
+        moviesBean.addMovie(new Movie("Zoolander", "Ben Stiller", "Comedy", 6, 2001));
+        moviesBean.addMovie(new Movie("Shanghai Noon", "Tom Dey", "Comedy", 7, 2000));
+
+        model.put("movies", moviesBean.getMovies());
+
+        return "setup";
+    }
+}
